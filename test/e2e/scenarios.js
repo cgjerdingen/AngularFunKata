19f018b91dd6f8a70d718a438bc9b56fe04059ba
Craig Gjerdingen <gjer0005@umn.edu>
2015-10-04 14:05:55 -0500

    Adding some Karma end to end testing

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -2,3 +2,33 @@
 
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
+describe('event registration app', function() {
+
+	describe('events page', function() {
+		beforeEach(function() {
+			browser.get('http://localhost:8000/events/');
+		})
+
+		it('Should have the correct title and first event', function() {
+			var list = element.all(by.repeater('event in events'));
+
+			expect(list.count()).toEqual(5);
+
+			var titleElement = element(by.binding('event.name'));
+			expect(titleElement.getText()).toEqual('Angular Boot Camp');
+		});
+	});
+
+	describe('events details page', function() {
+		beforeEach(function() {
+			browser.get('http://localhost:8000/event/1');
+		})
+
+		it('Should sort sessions by name', function() {
+			var list = element.all(by.repeater('session in event.sessions'));
+			var titleElement = list.first().element(by.binding('title'));
+			expect(titleElement.getText()).toEqual('Directives Masterclass');
+		});
+	});
+
+});
--- test/protractor-conf.js
+++ test/protractor-conf.js
@@ -1,8 +1,10 @@
 exports.config = {
+  seleniumAddress: 'http://localhost:4444/wd/hub/',
+
   allScriptsTimeout: 11000,
 
   specs: [
-    'e2e/*.js'
+    'e2e/**/*.js'
   ],
 
   capabilities: {
